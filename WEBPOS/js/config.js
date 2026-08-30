1f7b2a0924657d8e099408d23ce6a77a511c91ab
anshuljaipur <122553325+anshuljaipur@users.noreply.github.com>
2026-08-30 23:26:45 +0530

    Delete WEBPOS directory

--- WEBPOS/js/config.js
+++
@@ -1,9 +0,0 @@
-// PASTE YOUR GOOGLE APPS SCRIPT WEB APP URL HERE
-const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbxNEtZH-ugGQgTwcfDFNdJ8Thd46FxeZJ8emO4v3mqeCkhcQKqW0UQNDj63XitbNwE/exec", 
-    STORE_NAME: "GLACIER",
-    STORE_ADDRESS: "A-12, Janta Store, Jaipur",
-    STORE_PHONE: "9829610570",
-    INVOICE_PREFIX: "GLC-",
-    ROUND_OFF: true
-};
